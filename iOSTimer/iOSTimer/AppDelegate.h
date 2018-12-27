74fb9288eaf27faed3ec7c11d5b14ce8660779e8
huangxiaogang <573092608@qq.com>
2018-12-27 10:33:17 +0800

    新增初始化文件

---
+++ iOSTimer/iOSTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSTimer
+//
+//  Created by 黄小刚 on 2018/12/27.
+//  Copyright © 2018年 黄小刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSTimer/iOSTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSTimer
+//
+//  Created by 黄小刚 on 2018/12/27.
+//  Copyright © 2018年 黄小刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
